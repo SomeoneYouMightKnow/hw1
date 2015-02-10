f6dfb7bf448ee9c36300f581d96b64e51b21ccc0
Thomas Wong <twong3.1415926@gmail.com>
2015-02-10 08:55:01 -0500

    hw1

--- 01_recursion/Recursion.java
+++ 01_recursion/Recursion.java
@@ -26,35 +26,78 @@ public class Recursion{
 
 
     //len(s) –> returns the length of a string
-
+    public int length( String s ) {
+	if (s.equals("")) {
+	    return 0;
+	} else {
+	    return 1 + length( s.substring(1) );
+	}
+    }
 
 
     //count(s,c) –> counts the number of times character c occurs in String s.
-
+    public int count( String s , char c ) {
+	if ( s.length() == 0 ) {
+	    return 0;
+	}
+	if ( s.charAt(0) == c ) {
+	    return 1 + count( s.substring(1),c );
+	} else {
+	    return 0 + count( s.substring(1),c );
+	}
+    }
 
 
     //Do these coding bat problems:
     //codingbat.com/prob/p120015
-
+    //See fibonacci
 
 
     //codingbat.com/prob/p107330
+    public int bunnyEars2(int bunnies) {
+	if ( bunnies == 0 ) {
+	    return 0;
+	}
+	if ( bunnies % 2 == 0 ) {
+	    return 3 + bunnyEars2( bunnies - 1 );
+	} else {
+	    return 2 + bunnyEars2( bunnies - 1 );
+	}
+    }
 
 
 
     //codingbat.com/prob/p186177
+    public int strCount(String str, String sub) {
+	if ( str.length() < sub.length() ) {
+	    return 0;
+	}
+	if ( str.substring(0,sub.length()).equals(sub) ) {
+	    return 1 + strCount( str.substring(sub.length()),sub );
+	}
+	return strCount( str.substring(1),sub );
+    }
 
 
 
     //codingbat.com/prob/p163932
-
-
-
-    //codingbat.com/prob/p107330
+    public int sumDigits(int n) {
+	if ( n < 10 ) {
+	    return n;
+	} else { 
+	    return n % 10 + sumDigits( n / 10 );
+	}
+    }
 
 
 
     //codingbat.com/prob/p183394
+    public String allStar(String str) {
+	if ( str.length() <= 1 ) {
+	    return str;
+	}
+	return str.substring(0,1) + "*" + allStar( str.substring(1) );
+    }
 
 
 
