a2d3af42d2f9feaf2ee0af9bbc37d38e1ad53c8a
Thomas Wong <twong3.1415926@gmail.com>
2015-02-24 09:28:37 -0500

    toString, constructor, working on stomp

--- 03_USACO/Makelake.java
+++ 03_USACO/Makelake.java
@@ -7,27 +7,53 @@ public class Makelake {
     private int cols;
     private int elevation;
     private int N;
+    private Scanner sc = null;
     
     public Makelake() {
-	Scanner sc = null;
 	try {
-	    sc = new Scanner(new File(makelake.in));
+	    sc = new Scanner(new File("makelake.in"));
 	} catch (Exception e) {
 	    System.out.println("File not found");
 	    System.exit(0);
 	}
 	rows = sc.nextInt();
-	cols = sc.nextint();
+	cols = sc.nextInt();
 	elevation = sc.nextInt();
 	N = sc.nextInt();
+	field = new int[rows][cols];
 	for (int r = 0; r < rows; r++){
 	    for (int c = 0; c < cols; c++){
 		field[r][c] = sc.nextInt();
+		//System.out.println(sc.nextInt());
 	    }
+	    // System.out.println();
 	}
     }
 
+    public  String toString() {
+	String s = "";
+	for (int i = 0; i < field.length; i++) {
+	    for (int j = 0; j < field[0].length; j++ ) {
+		s = s + " " + field[i][j];
+	    }
+	    s = s + "\n";
+	}
+
+	return s;
+    }
+
+    public void stomp(int R_s, int C_s, int D_s) {
+	
+	
+	while (D_s > 0) {
+	    D_s--;
+	    stomp( R_s, C_s, D_s );
+	}
+    }
+    
     public static void main(String args[]){
 	Makelake l = new Makelake();
+        System.out.print(l);
+	l.stomp(2,2,2);
     }
 }
