a93beb50c6c2c362c9a4c74bc152a3804d1a60e0
Thomas Wong <twong3.1415926@gmail.com>
2015-02-26 09:27:46 -0500

    merge

--- 04_mergeSort/Mergesort.java
+++ 04_mergeSort/Mergesort.java
@@ -1,8 +1,8 @@
 import java.util.*;
+import java.io.*;
 
 public class Mergesort {
-    private ArrayList<Integer> deck = new ArrayList<Integer>();
-
+    
     public ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b){
     /*
       a and b are ArrayLists that are alread sorted from low to high.
@@ -12,24 +12,66 @@ public class Mergesort {
       discussed in class
     */
 	ArrayList<Integer> output = new ArrayList<Integer>();
-	//use a while loop so we can use i in the next part
-	for (int i = 0; i < a.size() && i < b.size(); i++) {
-	    if (a.get(i) < b.get(i)) {
-		output.add(a.get(i));
+	while (a.size()>0 && b.size()>0) {
+	    if (a.get(0) < b.get(0)) {
+		output.add(a.remove(0));
 	    } else {
-		output.add(b.get(i));
+		output.add(b.remove(0));
 	    }
 	}
-	if (a.size() >= b.size()) {
-	    
-	} else {
-	    for 
+	while (a.size()>0) {
+	    output.add(a.remove(0));
 	}
+	while (b.size()>0) {
+	    output.add(b.remove(0));
+	}
+        return output;
     }
 
+    public ArrayList<Integer> mergeSort(ArrayList<Integer> a) {
+	//a can be unsorted;
+	ArrayList<Integer> output, r1, r2;
+	r1 = new ArrayList<Integer>();
+	r2 = new ArrayList<Integer>();
+	for (int i = 0; i < a.size(); i++) {
+	    if (i<a.size()/2){
+		r1.add(a.get(i));
+	    } else {
+		r2.add(a.get(i));
+	    }
+	}
+	
+	merge(r1,r2);
+	
+	return output;
+    }
+
+    public int[] mergeArray( int[] a, int[] b) {
+	int[] output = new int[10];
+	
+	return output;
+    }
 
     public static void main(String args[]) {
 	Mergesort m = new Mergesort();
+	ArrayList<Integer> c, d, e;
+	c = new ArrayList<Integer>();
+	d = new ArrayList<Integer>();
+	e = new ArrayList<Integer>();
+	Random r = new Random();
+	for(int i = 1 ; i < 10 ; i++){
+	    c.add(i);
+	    d.add(2*i+1);
+	    e.add(r.nextInt());
+	}
+	
+	System.out.println("c: "+c);
+	System.out.println("d: "+d);
+	System.out.println("m: "+m.merge(c,d));
+	System.out.println();
+	System.out.println("e: "+e);
+	System.out.println("m: "+m.mergeSort(e));
+       
     }
     
 }
