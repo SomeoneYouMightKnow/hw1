7de7d495e3b05ce4b720b753da80815ff037eaaf
SomeoneYouMightKnow <twong3.1415926@gmail.com>
2015-03-05 15:32:48 -0500

    selection partition complete

--- 05_selection/Selection.java
+++ 05_selection/Selection.java
@@ -1,11 +1,33 @@
 public class Selection {
 
-    public void select( int k, int[] L, int si, int ei ){
-
+    public void select( /*int k,*/ int[] L, int si, int ei ){
+        int[] D = new int[L.length];
+        for (int i = 0; i < L.length; i++){
+            if (i == si) {
+                D[i] = L[i];
+            }   else {
+                i = si-1;
+            }
+        }
+        int save = L[si];
+        int i = si;
+        while (si < ei){
+            if (L[i] < save) {
+                D[si] = L[i];
+                si++;
+            } else {
+                D[ei] = L[i];
+                ei--;
+            }
+             i++;
+        }
+        D[si] = L[i];
+        }
     }
 
     public static void main(String[] args){
-
+        Selection s = new Selection();
+        s.select( [4, 6, 2, 7, 1, 8, 3, 9, 0, 5] , 3, 6);
     }
     
 }
