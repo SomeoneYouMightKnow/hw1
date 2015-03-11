e3c4689b6ee2766ba909f698e00a8179a235a114
Thomas Wong <twong3.1415926@gmail.com>
2015-03-11 09:08:20 -0400

    LinkedList

---
+++ 07_LinkedList/LinkedList.java
@@ -0,0 +1,8 @@
+public class LinkedList {
+    /*
+      A linked list is a collection of nodes that each contain data and a link
+      to the next node. The linked list must refer to the start node.
+     */
+
+
+}
