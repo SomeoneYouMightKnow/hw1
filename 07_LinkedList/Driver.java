f8f600a23b1494c61127a8d8029ae0a0ceedec09
Thomas Wong <twong3.1415926@gmail.com>
2015-03-12 09:07:25 -0400

    Print full LinkedList

--- 07_LinkedList/Driver.java
+++ 07_LinkedList/Driver.java
@@ -2,19 +2,39 @@ public class Driver {
     public static void main(String[] args) {
 	Node n = new Node("Hello");
 	//System.out.println(n);
-	System.out.println(n.getData());
+	//System.out.println(n.getData());
 
 	Node n2 = new Node("World");
-	System.out.println(n2.getData());
+	//System.out.println(n2.getData());
 
 	n.setNext(n2);
-	System.out.println(n.getNext());
+	//System.out.println(n.getNext());
 
 	n2.setData("pickle");
-	System.out.println(n.getNext());
+	//System.out.println(n.getNext());
 
-	n.getNext().setData("watermellon");
-	System.out.println(n.getNext());
+	n.getNext().setData("watermelon");
+	//System.out.println(n.getNext());
+
+	n.getNext().setNext(new Node("abc"));
+	//System.out.println(n2.getNext());
+
+	n.getNext().getNext().setNext(new Node("aaa"));
+	//System.out.println(n2.getNext().getNext());
+
+	Node tmp = new Node("first");
+	tmp.setNext(n);
+	n = tmp;
+
+	//System.out.println(n);
+	//System.out.println(n.getNext());
+
+	for(Node i = n; i != null; i = i.getNext()) {
+	    System.out.println(i);
+	}
+
+	
+	
     }
 
 
