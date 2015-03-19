633f89754350940b7121fccc35d692aa3567ae78
Thomas Wong <twong3.1415926@gmail.com>
2015-03-19 09:15:00 -0400

    ll2

--- 08_LList/Driver.java
+++ 08_LList/Driver.java
@@ -1,1 +1,53 @@
+import java.io.*;
+import java.util.*;
 
+public class Driver{
+    public static void main(String[] args){
+	//LList tests
+	///*
+	LList l = new LList();
+	l.add(1);
+	l.add(2);
+	l.add(3);
+	l.add(4);
+	l.add(5);
+	l.add(6);
+	System.out.println(l);
+	System.out.println(l.get(1));
+	l.insert(3,7);
+	System.out.println(l);
+	l.remove(6);
+	System.out.println(l);
+	l.remove2(3);
+	System.out.println(l);// */
+
+	//timing
+	/*
+	  ArrayList<Integer> al = new ArrayList<Integer>();
+	  for (int i=0;i<1000000;i++){
+	  al.add(i);
+	  }
+	  LList ll = new LList();
+	  for (int i=0;i<100000;i++){
+	  ll.add(i);
+	  }
+	
+	  long start;
+	  int sum;
+	  start = System.currentTimeMillis();
+	  sum = 0;
+	  for (int i=0;i<al.size(); i++){
+	  sum = sum + al.get(i);
+	  }
+	  System.out.println(System.currentTimeMillis()-start);
+	  //remains at around 8
+	  start = System.currentTimeMillis();
+	  sum = 0;
+	  for (int i=0;i<ll.len; i++){
+	  sum = sum + ll.get(i);
+	  }
+	  System.out.println(System.currentTimeMillis()-start); 
+	  //107 -> 11690 as you go from 10000 to 100000 
+	  */
+    }
+}
--- 08_LList/LList.java
+++ 08_LList/LList.java
@@ -1,1 +1,42 @@
+public class LList {
+    private Node l = null;
 
+    public void add(String s) {
+	Node tmp = new Node(s);
+	tmp.setNext(l);
+	l = tmp;
+	
+    }
+
+    public String toString() {
+	String s = "";
+	Node tmp;
+	for (tmp=l; tmp != null; tmp = tmp.getNext()) {
+	    s = s + tmp + " --> ";
+	}
+	s = s + "null";
+	return s;
+    }
+
+    public String find( int n ) {
+	Node output = l;
+	for (int i = 0; i < n; i++) {
+	    output = output.getNext();
+	}
+	return output.getData();
+    }
+
+    public void insert( int n, String s ) {
+	Node insert = new Node(s);
+	Node temp = l;
+	for (int i = 0; i < n-1; i++) {
+	    temp = temp.getNext();
+	}
+	if ( n == 0 ) {
+		insert.setNext(l);
+	} else {
+		insert.setNext(temp.getNext());
+		temp.setNext(insert);
+    	}
+    }
+}
