ff7e5500be08595d0d423610cf774a32527a3515
Thomas Wong <twong3.1415926@gmail.com>
2015-03-25 09:01:24 -0400

    hw10

---
+++ 10_myStack/myStack.java
@@ -0,0 +1,36 @@
+public class myStack<E>{
+    // You decide how the node(s) are declared
+
+    // make whatever constructor(s) you need
+
+    private Node<E> top;
+
+    public myStack(E s) {
+	first = new Node(s);
+	top = new Node();
+	top.setNext(first);
+    }
+
+    public void push(E data){
+        // add something to the top
+	Node<E> insert = new Node<E>(data);
+        insert.setNext(top.getNext().getNext());
+	top.setNext(insert);
+    }
+
+    public E pop(){
+        // remove and return the top item from the stack
+	Node<E> tmp = top;
+        top.setNext(top.getNext().getNext());
+	return tmp;
+    }
+
+    public boolean empty(){
+	return top.getNext() == null;
+    }
+
+    public E top() {
+        // return the top item from the stack
+	return top.getNext().getData();
+    }
+}
