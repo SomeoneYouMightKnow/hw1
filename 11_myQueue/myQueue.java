fad73dc57a302501aa7e83623f0f6c64e626256f
Thomas Wong <twong3.1415926@gmail.com>
2015-03-26 09:26:26 -0400

    dequeue isn't printing

--- 11_myQueue/myQueue.java
+++ 11_myQueue/myQueue.java
@@ -1,30 +1,51 @@
 public class myQueue<E>{
     // You decide how the node(s) are declared
-    Node<E> front, back;
+    Node<E> front, back, first;
 
-    // make whatever constructor(s) you need
-    public myQueue(E s) {
+    // make whatever constructor(s) you need\
+    public myQueue() {
 	front = new Node<E>();
-	Node<E> first = new Node<E>(s);
+	first = new Node<E>();
 	back = new Node<E>();
 	front.setNext(first);
 	first.setNext(back);
+	first.setPrev(front);
+	back.setPrev(first);
     }
 	
     public void enqueue(E data){
         // add something to the tail/last
 	Node<E> insert = new Node<E>(data);
+	insert.setNext(back);
+	back.getPrev().setNext(insert);
+	insert.setPrev(back.getPrev());
+	back.setPrev(insert);
     }
 
     public E dequeue(){
         // remove and return the head/front item from the stack
+	Node<E> tmp = first;
+	first = first.getNext();
+	return tmp.getData();
     }
 
     public boolean empty(){
-	
+	return first.getData() == null;
     }
 
     public E head() {
         // return the first item in the queue
+	return first.getData();
     }
+
+    public String toString(){
+	String s = "FIRST: " + first;
+	for (Node<E> tmp = first.getNext(); tmp!=null; tmp=tmp.getNext()){
+	    s += " --> " + tmp;
+	}
+	s += " FRONT: " + front + " BACK: " + back;
+	return s;
+    }
+
+    
 }
