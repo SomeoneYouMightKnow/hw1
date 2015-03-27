4f6032f682556f6a0573fe2618fcc18f5745b8b8
Thomas Wong <twong3.1415926@gmail.com>
2015-03-27 08:57:39 -0400

    myQueue done, might remove first

--- 11_myQueue/Driver.java
+++ 11_myQueue/Driver.java
@@ -1,11 +1,13 @@
 public class Driver {
     public static void main(String[] args){
 	myQueue<Integer> s = new myQueue<Integer>();
+	System.out.println(s);
 	System.out.println(s.empty());
 	for (int n = 0; n < 10; n++){
 	    s.enqueue(n);
 	    System.out.println(s);
 	}
+	System.out.println(s.empty());
 	while (!s.empty()){
 	    System.out.println(s.dequeue());
 	    System.out.println(s);
--- 11_myQueue/myQueue.java
+++ 11_myQueue/myQueue.java
@@ -2,10 +2,15 @@ public class myQueue<E>{
     // You decide how the node(s) are declared
     Node<E> front, back, first;
 
-    // make whatever constructor(s) you need\
+    // make whatever constructor(s) you need
+
     public myQueue() {
+	this(null);
+    }
+    
+    public myQueue(E s) {
 	front = new Node<E>();
-	first = new Node<E>();
+	first = new Node<E>(s);
 	back = new Node<E>();
 	front.setNext(first);
 	first.setNext(back);
@@ -15,17 +20,23 @@ public class myQueue<E>{
 	
     public void enqueue(E data){
         // add something to the tail/last
+	if (empty()) {
+	    first.setData(data);
+	} else {
 	Node<E> insert = new Node<E>(data);
 	insert.setNext(back);
 	back.getPrev().setNext(insert);
 	insert.setPrev(back.getPrev());
 	back.setPrev(insert);
+	}
     }
 
     public E dequeue(){
         // remove and return the head/front item from the stack
 	Node<E> tmp = first;
 	first = first.getNext();
+	first.setPrev(front);
+	front.setNext(first);
 	return tmp.getData();
     }
 
@@ -39,11 +50,11 @@ public class myQueue<E>{
     }
 
     public String toString(){
-	String s = "FIRST: " + first;
-	for (Node<E> tmp = first.getNext(); tmp!=null; tmp=tmp.getNext()){
-	    s += " --> " + tmp;
+	String s = "";
+	for (Node<E> tmp = front; tmp!=back; tmp=tmp.getNext()){
+	    s += tmp + " --> ";
 	}
-	s += " FRONT: " + front + " BACK: " + back;
+	s += back;
 	return s;
     }
 
