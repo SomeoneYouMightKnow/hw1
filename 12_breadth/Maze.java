ac5c63dbafad275e4ccc420100fe687386bb90c6
Thomas Wong <twong3.1415926@gmail.com>
2015-04-02 09:22:10 -0400

    added Maze.java

--- 12_breadth/Maze.java
+++ 12_breadth/Maze.java
@@ -9,50 +9,46 @@ public class Maze{
     private boolean solved = false;
 
     
-    public void delay(int n){
+    public void delay(int n) {
 	try {
 	    Thread.sleep(n);
 	} catch (Exception e) {}
     }
     
-    public Maze() 
-    {
+    public Maze() {
 	maxX=40;
 	maxY=20;
 	board = new char[maxX][maxY];
 				
 	try {
-						
 	    Scanner sc = new Scanner(new File("maze.dat"));
 	    int j=0;
-	    while (sc.hasNext())
-		{
+	    while (sc.hasNext()) {
 		    String line = sc.nextLine();
-		    for (int i=0;i<maxX;i++)
-			{
+		    for (int i=0;i<maxX;i++) {
 			    board[i][j] = line.charAt(i);
 			}
 		    j++;
 		}
 	}
-	catch (Exception e)
-	    {
-	    }
-				
+	catch (Exception e) {}
     }
 		
-    public String toString()
-    {
+    public String toString() {
 	String s = "[2J\n";
-				
-	for (int y=0;y<maxY;y++)
-	    {
-		for (int x=0;x<maxX;x++)
-		    s = s +board[x][y];
-		s=s+"\n";
+	for (int y=0;y<maxY;y++) {
+	    for (int x=0;x<maxX;x++) {
+		s = s +board[x][y];
 	    }
+	    s=s+"\n";
+	}
 	return s;
     }
 
-    
+    public static void main(String[] args){
+	Maze m = new Maze();
+	System.out.println(m);
+	m.solve(1,1);
+	System.out.println(m);
+    }
 }
