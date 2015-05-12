0a13a5778b901ff6b1c025a441227feb1f2ef9eb
Thomas Wong <twong3.1415926@gmail.com>
2015-05-12 09:23:46 -0400

    binary tree

--- 15_btree/BTree.java
+++ 15_btree/BTree.java
@@ -2,57 +2,76 @@ public class BTree {
     private Node root;
 
     public BTree() {
-	    this(null);
+	this(null);
     }
 
     public BTree( Node n ) {
-	    root = n;
+	root = n;
     }
 
     public int numNodes( Node t ) {
-	    if ( t == null ) {
-	        return 0;
-	    } else {
+	if ( t == null ) {
+	    return 0;
+	} else {
     	    return 1 + numNodes( t.getLeft() ) + numNodes( t.getRight() );
-	        //sumOfValues is t.getData()+sumOfValues(left)+sumOfValues(right)
-	    }
+	    //sumOfValues is t.getData()+sumOfValues(left)+sumOfValues(right)
+	}
     }
 
     public int maxValue( Node t ) {
-	    int max = t.getData();
-	    if ( t != null ) {
-	        if ( max <= maxValue(t.getLeft()) ) {
-		        max = maxValue(t.getLeft());
-	        }
-	        if ( max <= maxValue(t.getRight()) ) {
-		        max = maxValue(t.getRight());
-	        }
+	int max = t.getData();
+	if ( t != null ) {
+	    if ( max <= maxValue(t.getLeft()) ) {
+		max = maxValue(t.getLeft());
 	    }
-	    return max;
+	    if ( max <= maxValue(t.getRight()) ) {
+		max = maxValue(t.getRight());
+	    }
+	}
+	return max;
     }
 
     public int height( Node t ) {
-	    if ( t == null ) {
-	        return 0;
+	if ( t == null ) {
+	    return 0;
+	} else {
+	    if (height(t.getRight()) > height(t.getLeft())) {
+		return 1 + height(t.getRight());
 	    } else {
-	        if (height(t.getRight()) > height(t.getLeft())) {
-	            return 1 + height(t.getRight());
-	        } else {
-	            return 1 + height(t.getLeft());
-	        }
+		return 1 + height(t.getLeft());
 	    }
+	}
     }
 
     public void splitDupes( Node t ) {
-
-
+	if ( t.getNext().getData() == t.getData() ) {
+	    Node tmp = new Node( t.getData()-1 );
+	    tmp.setNext( t.getNext() );
+	    t.setNext( tmp );
+	} else {
+	    splitDupes( t.getLeft() );
+	    splitDupes( t.getRight() );
+	}
     }
 
-    public int longest( Node t ) {
-	
-	
+    public int diameter( Node t ) {
+	if ( t == null ) {
+	    return 0;
+	} else {
+	    int p1 = height( t.getLeft() ) + height( t.getRight() ) + 2;
+	    int p2 = diameter( t.getLeft() );
+	    int p3 = diameter( t.getRight() );
+	    if  ( p1 > p2 && p1 > p3 ) {
+		return p1;
+	    } else if ( p2 > p1 && p2 > p3 ) {
+		return p2;
+	    } else {
+		return p3;
+	    }
+	}
     }
 
+    
     
     public String traverse( Node t ) {
 	if ( t == null ) {
@@ -71,7 +90,6 @@ public class BTree {
 
     public static void main(String args[]) {
 	BTree t = new BTree();
-
     }
 
 
